43c47abfbfb5be97acef89910a74b56ecd0ee201
Gavin Merry <147446785+gavinmerry@users.noreply.github.com>
2025-03-31 13:17:04 +0100

    Update victim_vm_setup.sh

--- victim_vm_setup.sh
+++ victim_vm_setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
 echo "[*] Setting up Victim VM..."
+
+# Prompt user for the DNS Forwarder IP
+read -p "Enter the IP address of the DNS Forwarder: " FORWARDER_IP
 
 # Install Python and dnspython
 sudo apt update
 sudo apt install -y python3-pip
 pip3 install dnspython
 
-# Set DNS to forwarder (replace <FORWARDER_IP>)
-sudo sed -i 's/#DNS=.*/DNS=<FORWARDER_IP>/' /etc/systemd/resolved.conf
-sudo sed -i 's/#FallbackDNS=.*/FallbackDNS=/' /etc/systemd/resolved.conf
+# Set DNS to the Forwarder IP
+sudo sed -i "s/^#*DNS=.*/DNS=${FORWARDER_IP}/" /etc/systemd/resolved.conf
+sudo sed -i "s/^#*FallbackDNS=.*/FallbackDNS=/" /etc/systemd/resolved.conf
 sudo systemctl restart systemd-resolved
 
-echo "[+] Victim setup complete. DNS set to forwarder."
+echo "[+] Victim setup complete. DNS set to forwarder at ${FORWARDER_IP}."
